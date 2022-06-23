98c5c96d7b7ed31b31829ac1bc0fab528f07a9f8
MatiaPercic <mapercic@unipu.hr>
2022-06-23 12:45:57 +0200

    setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Ovo je mock alikacije ScoutHelp"/>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
